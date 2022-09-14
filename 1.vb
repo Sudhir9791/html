33e14ad024f99ce44f1af5cd8bc4eaa3ccf07be0
Sudhir9791 <106509959+Sudhir9791@users.noreply.github.com>
2022-09-14 09:18:36 +0530

    Add files via upload

---
+++ 1.vb
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <title>
+            Sample
+        </title>
+        <h1>SUDHIR S</h1>
+    </head>
+    <body>
+      <p>Student</p>  
+      <p>+91 9791687878</p>
+      <p><u>sudhirsubramanian003@gmail.com</u></p>
+      <h3>Education</h3>
+         <ul style="list-style-type: square">
+            <li><h4>School</h4></li>
+            <ol>
+                <li>VVMHSS</li>
+                <ul style="list-style-type: circle">
+                    <li>Located in Coimbatore</li>
+                </ul>
+            </ol>
+            
+            <li><h4>College</h4></li>
+            <ol>
+                <li>SKCT</li>
+                <ul style="list-style-type: circle">
+                    <li>Located in Coimbatore</li>
+                </ul>
+            </ol>
+            
+         </ul>
+         <table style="width:100%">
+            <style>
+                table,
+                th,
+                td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+                </style>
+            <caption>IRC Courses</caption>
+            <tr>
+                <th>Course Name</th>
+                <th>Trainer</th>
+                <th>Hall</th>
+                <th>Company</th>
+              </tr>
+              <tr>
+                <th rowspan="2">C++ with DS</th>
+                <td>Dharani</td>
+                <th rowspan="4">Drawing hall</td>
+                <th rowspan="6">iamneo</td>
+            </tr>
+            <tr>
+                <td>Koutheesh</td>
+                
+            </tr>
+            <tr>
+                <tr rowspan="2">Java</tr>
+                <td>Dharani</td>
+            </tr>
+            <tr>
+                <td>Koutheesh</td>
+            </tr>
+            <tr>
+                <th>SQL</th>
+                <td>Sakthivel</td>
+                <td>IC SF</td>
+            </tr>
+            <tr>
+                <th>Web tech</th>
+                <td>Koutheesh</td>
+                <td>IC GF</td>
+            </tr>
+
+         </table>
+          <p><h4>Reference URL</h4></p>
+          <a href="https://github.com/Sudhir9791">For github profile click here!</a>
+          <p>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLPQRH1KwH5D-wVcWjh-NA-hRHfEXWCTC8RA&usqp=CAU" alt="Virat Kohli">
+        </p>
+    </body>
+</html>
